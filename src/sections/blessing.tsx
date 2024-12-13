410769b1cc6373fc48ae8c2c3f4aedf6e254798b
sertnayong <nuannimprasert@gmail.com>
2024-12-13 22:34:34 +0700

    change wish to csv

--- src/sections/blessing.tsx
+++ src/sections/blessing.tsx
@@ -4,6 +4,7 @@ import Card from "@/components/cardBlessing";
 import OTPModal from "@/components/modalOTP";
 import axios from "axios";
 import { useSectionInView } from "@/lib/useInView";
+import Papa from "papaparse";
 
 type Props = {};
 type Form = {
@@ -15,21 +16,23 @@ type Form = {
 const Blessing = () => {
   const { ref } = useSectionInView("#blessing", 0.1)
 
-  const [forms, setForms] = useState<Form[]>([]);
+  const [forms, setForms] = useState([]);
 
   // Fetch forms on page load
-  useEffect(() => {
-    fetchForms();
-  }, []);
+  // useEffect(() => {
+  //   fetchForms();
+  // }, []);
 
-  const fetchForms = async () => {
-    try {
-      const response = await axios.get("/api/forms");
-      setForms(response.data.forms);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const fetchForms = async () => {
+  //   try {
+  //     const response = await axios.get("/api/forms");
+  //     setForms(response.data.forms);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+
 
   const containerVariants = {
     hidden: { opacity: 0 },
@@ -40,6 +43,18 @@ const Blessing = () => {
     hidden: { opacity: 0, y: 20 },  // Start below and invisible
     visible: { opacity: 1, y: 0, transition: { duration: 0.5 } }, // Slide to position
   };
+
+  useEffect(() => {
+    // Load CSV file
+    Papa.parse("/data/wish.csv", {
+      download: true,
+      header: true, // Parse the first row as headers
+      complete: (result:any) => {
+        setForms(result?.data);
+      },
+    });
+  }, []);
+
   return (
     <section
       ref={ref}
@@ -55,15 +70,15 @@ const Blessing = () => {
             Wedding Wish
           </p>
         </div>
-        <div className="relative mb-4">
+        {/* <div className="relative mb-4">
           <OTPModal fetchForms={fetchForms} />
-        </div>
+        </div> */}
 
         <form className="flex flex-col justify-center">
           <div className="h-96 overflow-y-auto border p-4  space-y-4 bg-white">
             <p className="block text-lg font-semibold">คำอวยพร</p>
             <div className="flex flex-wrap justify-center content-start gap-4">
-              {forms.map((item) =>
+              {forms.map((item:any) =>
                 item.name ? (
                   <div
                   >
